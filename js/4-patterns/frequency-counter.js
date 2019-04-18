85376a5bae16a7daa8b81cf3ad28130d8eaa4274
Bion Howard <bion@bitpharma.com>
2019-04-18 18:51:34 -0400

    add frequency-counter.js

---
+++ js/4-patterns/frequency-counter.js
@@ -0,0 +1,32 @@
+// same - write a function which accepts two arrays
+// and returns true if the 2nd array contains the square of all items in 1st
+
+const same = (arr1, arr2) => {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  let counter1 = {};
+  let counter2 = {};
+  for (let val of arr1){
+    counter1[val] = (counter1[val] || 0) + 1;
+  }
+  for (let val of arr2){
+    counter2[val] = (counter2[val] || 0) + 1;
+  }
+  for (let key in counter1){
+    if (!(key ** 2 in counter2)){
+      return false;
+    }
+    if (counter2[key**2] !== counter1[key]){
+      return false;
+    }
+  }
+  return true;
+}
+
+const one = [1,2,3];
+const two = [1,4,9];
+const three = [1,1,1];
+
+console.log(same(one, two)) // true
+console.log(same(one, three)) // false
